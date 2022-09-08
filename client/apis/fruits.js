5b82313a42d0a52a030eebc50460db64ea86dccd
Shantaliah Filipo <shantaliah.filipo@gmail.com>
2022-09-08 20:14:29 +1200

    Rename

--- client/apis/fruits.js
+++
@@ -1,9 +0,0 @@
-import request from 'superagent'
-
-const rootUrl = '/api/v1'
-
-export function getActivity() {
-  return request.get(rootUrl + '/activity').then((res) => {
-    return res.body
-  })
-}
