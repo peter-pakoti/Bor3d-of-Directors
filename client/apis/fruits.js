858074a73e6ad15f2879700c82be59361316be82
Shantaliah Filipo <shantaliah.filipo@gmail.com>
2022-09-08 19:55:59 +1200

    create api getActivity function to be called in other files

--- client/apis/fruits.js
+++ client/apis/fruits.js
@@ -2,8 +2,8 @@ import request from 'superagent'
 
 const rootUrl = '/api/v1'
 
-export function getFruits() {
-  return request.get(rootUrl + '/fruits').then((res) => {
-    return res.body.fruits
+export function getActivity() {
+  return request.get(rootUrl + '/activity').then((res) => {
+    return res.body
   })
 }
