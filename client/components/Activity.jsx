1612c1a1c60172b560efa7a5ae1d7b488c989d6a
Shantaliah Filipo <shantaliah.filipo@gmail.com>
2022-09-08 20:05:55 +1200

    Create function to show activity

--- client/components/Activity.jsx
+++ client/components/Activity.jsx
@@ -2,3 +2,11 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 
 // need to import a fetchActivity function
+
+function ShowActivity() {
+  const dispatch = useDispatch()
+
+  return <div></div>
+}
+
+export default ShowActivity
