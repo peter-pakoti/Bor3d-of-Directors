75d48cfe0ca86aad523de9f38e31eabf9e35139a
Shantaliah Filipo <shantaliah.filipo@gmail.com>
2022-09-08 20:30:28 +1200

    Change from set fruits to set activity

---
+++ client/reducers/activity.js
@@ -0,0 +1,15 @@
+import { SET_ACTIVITY } from '../actions'
+
+const initialState = []
+
+const reducer = (state = initialState, action) => {
+  const { type, payload } = action
+  switch (type) {
+    case SET_ACTIVITY:
+      return payload
+    default:
+      return state
+  }
+}
+
+export default reducer
