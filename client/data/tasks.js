2ecfb1fee860fbe38ce69361d9a6a07db1046ba6
Shantaliah Filipo <shantaliah.filipo@gmail.com>
2022-09-09 13:54:31 +1200

    Add more tasks

--- client/data/tasks.js
+++ client/data/tasks.js
@@ -1,8 +1,13 @@
 export const tasks = [
   'Mow the lawns',
+  'Stress-eat a Milky bar so Gerard will let you out of group projects',
   'Have dinner with Jacinda',
   'Get rinsed',
   'Smoke Usain Bolt in a race',
   'Go for a cycle with Lance Armstrong',
   'Party with the HP Boyz',
+  'Get Jiggy with it',
+  'Listen to Breaking Free on repeat',
+  'Go swimming with the dolphins',
+  'Inhale a block of chocolate',
 ]
